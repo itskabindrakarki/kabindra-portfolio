2f9ba444a8af18b21090e603824d087954aa9a44
Kabindra Karki <karkikabindra98@gmail.com>
2026-06-01 08:55:53 +0545

    Update next.config.ts with output and image settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "https://github.com/itskabindrakarki/kabindra-portfolio.git", // repository name
+  assetPrefix: "/kabindra-portfolio/",
 };
 
 export default nextConfig;
